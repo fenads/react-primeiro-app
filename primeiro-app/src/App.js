938251c250bbb2fe519b7afcdcc32c9b48e6ec54
Luiz Fernando <ofenads@gmail.com>
2024-01-04 20:12:31 -0300

    Criacao da tela Inicial

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Rotas from './routes';
+import GlobalStyles from './styles/global'
 
 export default function App() {
 
   return (
+    <>
+    <GlobalStyles />
     <Rotas />
+    </>
   );
 }
--- primeiro-app/src/pages/Main/index.js
+++ primeiro-app/src/pages/Main/index.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaGithub, FaPlus } from 'react-icons/fa'
+
+//eslint-disable-next-line
+import { Container, Form, SubimitButtom } from "./styles";
 
 export default function Main(){
+
+  const [newRepo, setNewRepo] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    console.log(newRepo);
+  }
+
+  function handleinputChange(e){
+    setNewRepo(e.target.value);
+  }
+
   return(
-    <h1>Main</h1>
+
+    <Container>
+      <h1>
+        <FaGithub
+        size={25}
+        />
+        Meus Repositorios
+      </h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="Adicionar Repositorio"
+        value={newRepo}
+        onChange={handleinputChange}
+        />
+        
+        <SubimitButtom>
+          <FaPlus
+          color="#FFF"
+          size={14}
+          />
+        </SubimitButtom>
+      
+      </Form>
+    </Container>
   )
 }
